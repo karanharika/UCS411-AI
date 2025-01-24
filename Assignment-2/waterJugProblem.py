871c387fa7b65f14cb4fe5f1d2ffe8794c978007
karanharika <55457934+karanharika@users.noreply.github.com>
2025-01-24 22:31:38 +0530

    waterJugProblem.py working and can also put different initial and goal states.

--- Assignment-2/waterJugProblem.py
+++ Assignment-2/waterJugProblem.py
@@ -1,13 +1,16 @@
 """ Written by: Karanveer Singh Harika
     Roll No: 102483034
-    This code tackles with the two water jug transfer
+    This code tackles with the two water jug transfer problem
 """
 import copy
 
+jugA = 0
+jugB = 0
 
-def check_for_2liter(s):
-    """Returns True if 4L jug has 2L of water"""
-    if s[0] == 2:
+
+def check_for_2liter(s, g):
+    """Returns True if 4L jug has 2L of water, i.e., if goal state is achieved."""
+    if s == g:
         return True
     else:
         return False
@@ -15,135 +18,103 @@ def check_for_2liter(s):
 
 def water_source(s, jug):
     """Fills 4L jug from water source if jug=4 is passed or 3L jug from water source if jug=3 is passed."""
-    print("ws")
     temp = copy.deepcopy(s)
-    if jug == 4:
-        temp[0] = 4
-    if jug == 3:
-        temp[1] = 3
-    return temp
+    if (jug == jugA) & (s[0] < jugA):
+        temp[0] = jugA
+        return temp
+    if (jug == jugB) & (s[1] < jugB):
+        temp[1] = jugB
+        return temp
 
 
 def empty_jug(s, jug):
     """Empties jug based on what jug argument is passed."""
-    print("empty")
     temp = copy.deepcopy(s)
-    if jug == 4 & s[0] != 0:
+    if jug == jugA & s[0] != 0:
         temp[0] = 0
-    if jug == 3 & s[1] != 0:
+        return temp
+    if jug == jugB & s[1] != 0:
         temp[1] = 0
-    return temp
+        return temp
 
 
 def move_water_3_to_4(s):
-    """Transfer water from 3L jug to 4L jug without overflowing"""
-    print("3->4")
+    """Transfer water from 3L jug to 4L jug without overflowing."""
     # water_in_4 = s[0]
     # water_in_3 = s[1]
     temp = copy.deepcopy(s)
-    transfer_amount = 4 - temp[0]
-    temp[0] = temp[0] + transfer_amount
-    temp[1] = temp[1] - transfer_amount
-    return temp
+    if s[1] != 0:
+        # transfer_amount = 4 - temp[0]
+        transfer_amount = min(s[1], jugA - s[0])
+        temp[0] = temp[0] + transfer_amount
+        temp[1] = temp[1] - transfer_amount
+        return temp
 
 
 def move_water_4_to_3(s):
-    print("4->3")
+    """Transfer water from 4L jug to 3L jug without overflowing."""
     temp = copy.deepcopy(s)
-    transfer_amount = 3 - temp[1]
-    temp[1] = temp[1] + transfer_amount
-    temp[0] = temp[0] - transfer_amount
-    return temp
+    if s[0] != 0:
+        transfer_amount = min(s[0], jugB - s[1])
+        temp[1] = temp[1] + transfer_amount
+        temp[0] = temp[0] - transfer_amount
+        return temp
 
 
 def search(s, g):
     """Finds goal state starting from initial state"""
     q = []
-    count = 0
-    if check_for_2liter(s):
+    if check_for_2liter(s, g):
         print("Found goal state!")
         return
     else:
-        while count <= 10:
-
-            s1 = water_source(s, 3)
-            q.append(s1)
-            s2 = water_source(s, 4)
-            q.append(s2)
+        while 1:
+            s1 = water_source(s, jugB)
+            if s1 is not None:
+                q.append(s1)
+            s2 = water_source(s, jugA)
+            if s2 is not None:
+                q.append(s2)
             s3 = move_water_3_to_4(s)
-            q.append(s3)
+            if s3 is not None:
+                q.append(s3)
             s4 = move_water_4_to_3(s)
-            q.append(s4)
-            s5 = empty_jug(s, 3)
-            q.append(s5)
-            s6 = empty_jug(s, 4)
-            q.append(s6)
-            print("Q = ", q)
-            count += 1
+            if s4 is not None:
+                q.append(s4)
+            s5 = empty_jug(s, jugB)
+            if s5 is not None:
+                q.append(s5)
+            s6 = empty_jug(s, jugA)
+            if s6 is not None:
+                q.append(s6)
+            # print("Queue = ", q)
             s = q[0]
             del q[0]
-            if check_for_2liter(s):
+            if check_for_2liter(s, g):
                 print("Found goal state!")
                 print(s)
                 return
 
 
-def transfer_water(to_jug_cap, from_jug_cap, desired):
-    """Returns the number of steps after transferring water from jugA to jugB."""
-    # Initialize current amount of water
-    # in source and destination jugs
-    from_jug = from_jug_cap
-    to_jug = 0
-
-    # Initialize steps required
-    step = 1
-    while (from_jug is not desired) and (to_jug is not desired):
-
-        # Find the maximum amount that can be
-        # poured
-        temp = min(from_jug, to_jug_cap - to_jug)
-
-        # Pour 'temp' liter from 'from_jug' to 'to_jug'
-        to_jug = to_jug + temp
-        from_jug = from_jug - temp
-
-        step = step + 1
-        if (from_jug == desired) or (to_jug == desired):
-            break
-
-        # If first jug becomes empty, fill it
-        if from_jug == 0:
-            from_jug = from_jug_cap
-            step = step + 1
-
-        # If second jug becomes full, empty it
-        if to_jug == to_jug_cap:
-            to_jug = 0
-            step = step + 1
-
-    return step
-
-
 def main():
-    # n = 3
-    # m = 5
-    # d = 4
-    n = 4
-    m = 3
-    d = 2
-    """Representation of state = [4 liter jug, 3 liter jug] ,i.e.,  
-            s[0] = 4 liter jug
-            s[1] = 3 liter jug"""
+    """main function"""
+    global jugA
+    jugA = 4
+    global jugB
+    jugB = 3
+    """Representation of state in list format = [4 liter jug, 3 liter jug] ,i.e.,  
+                                                s[0] = 4 liter jug
+                                                s[1] = 3 liter jug"""
+    s = [0, 0]  # Initial State
+    g = [2, 0]  # Goal State
 
-    s = [0, 0]
-    g = [2, 0]
+    print(f"Initial state: \n"
+          f"\t{s}\n"
+          f"\t^   ^\n"
+          f"\t|   |\n"
+          f"\t{jugA}L, {jugB}L")
 
     search(s, g)
-
-    # s1 = transfer_water(n, m, d)
-    # s2 = transfer_water(m, n, d)
-
-    # print('Number of steps required is', s1, ' or ', s2)
 
 
 if __name__ == '__main__':
